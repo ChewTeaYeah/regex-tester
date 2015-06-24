0c7d54e9a2b81871026a59cff3598695df609d1e
Mihir Patel <mpatel88@gmail.com>
2015-06-24 07:41:40 -0400

    Adding a basic frontend which hits the regex service

--- public_assets/scripts/app.js
+++ public_assets/scripts/app.js
@@ -1,1 +1,34 @@
-angular.module('regexTester', ['ui.bootstrap']);
+var app = angular.module('regexTester', ['ui.bootstrap']);
+
+app.service('regexTesterService', ['$q', '$http', function($q, $http){
+	var svc = {};
+
+	svc.testExpr = function(regexStr, regexOpts, testString){
+		var postData = {
+			regex: regexStr,
+			regex_options: regexOpts,
+			test_string: testString
+		};
+
+		var deferred = $q.defer();
+		$http.post('/test_expr', postData).then(function success(result){
+			deferred.resolve(result.data);
+		}, function error(response){
+			deferred.reject(response);
+		});
+
+		return deferred.promise;
+	};
+
+	return svc;
+}]);
+
+app.controller('regexTesterController', ['$scope', 'regexTesterService', function($scope, regexTesterService){
+	$scope.runTest = function(){
+		regexTesterService.testExpr($scope.regex, $scope.regexOpts, $scope.testString).then(function success(result){
+			$scope.testResult = JSON.stringify(result);
+		}, function error(response){
+			$scope.testResult = JSON.stringify(response.data);
+		});
+	};
+}]);
