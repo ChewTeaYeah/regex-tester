bdaa1cec91704ca0e529fa41340bb42db13b3ccd
Mihir Patel <mpatel88@gmail.com>
2015-06-24 22:24:17 -0400

    - Styling the front end - Only running the regex test if both regex and test string have values - Adding checkboxes for the regex options which both are filled by and provide data to the options text box

--- public_assets/scripts/app.js
+++ public_assets/scripts/app.js
@@ -24,11 +24,68 @@ app.service('regexTesterService', ['$q', '$http', function($q, $http){
 }]);
 
 app.controller('regexTesterController', ['$scope', 'regexTesterService', function($scope, regexTesterService){
-	$scope.runTest = function(){
-		regexTesterService.testExpr($scope.regex, $scope.regexOpts, $scope.testString).then(function success(result){
+	$scope.regex = "";
+	$scope.regexOptsText = "";
+	$scope.testString = "";
+
+	var runTest = function(){
+		if (!angular.isString($scope.regex) || $scope.regex.length === 0 
+			|| !angular.isString($scope.testString) || $scope.testString.length === 0){
+			$scope.testResult = "";
+			return;
+		}
+
+		regexTesterService.testExpr($scope.regex, $scope.regexOptsText, $scope.testString).then(function success(result){
 			$scope.testResult = JSON.stringify(result);
 		}, function error(response){
 			$scope.testResult = JSON.stringify(response.data);
 		});
 	};
+
+	$scope.regexOptions = {
+		i: false,
+		m: false,
+		x: false
+	};
+
+	$scope.$watchCollection('regexOptions', function(newVal, oldVal){
+		angular.forEach($scope.regexOptions, function(value, key){
+			if (value){
+				var re = new RegExp(key);
+				if (!re.test($scope.regexOptsText)){
+					$scope.regexOptsText += key;
+				}
+			} else {
+				$scope.regexOptsText = $scope.regexOptsText.replace(key, "");
+			}
+		});
+	});
+
+	$scope.$watchGroup(['regex', 'regexOptsText', 'testString'], function(newVals, oldVals){
+		if (newVals[0] !== oldVals[0] || newVals[1] !== oldVals[1] || newVals[2] !== oldVals[2]){
+			runTest();
+		}
+	});
+
+	var validOptsRegex = /^([mxi]?){0,3}$/i;
+	$scope.$watch('regexOptsText', function(newVal, oldVal){
+		if (newVal !== oldVal && angular.isString(newVal)){
+			// TODO: augment this if statement to also ensure all chars in input are unique
+			if (newVal.length <= 3 && validOptsRegex.test(newVal)){
+				$scope.regexOptsText = newVal.toLowerCase();
+			} else {
+				$scope.regexOptsText = oldVal;
+			}
+
+			angular.forEach($scope.regexOptions, function(value, key){
+				var re = new RegExp(key, "i");
+
+				if (re.test($scope.regexOptsText)){
+					$scope.regexOptions[key] = true;
+				} else {
+					$scope.regexOptions[key] = false;
+				}
+			});
+		}
+	});
 }]);
